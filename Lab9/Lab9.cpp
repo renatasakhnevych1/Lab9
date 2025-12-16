e3cf44c46e35d17634eef596289db02d9d16c9d5
Renata <nway2419@gmail.com>
2025-12-16 15:43:49 +0100

    Dodaj pliki projektów.

---
+++ Lab9/Lab9.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+
+//Zad1
+//struct Struktura {
+//	int x, y;
+//};
+
+//Zad2
+//struct Prostokat {
+//	double a, b;
+//}obiekt1;
+//
+//double Pole(struct Prostokat) {
+//	double pole;
+//	pole = obiekt1.a * obiekt1.b;
+//	return pole;
+//}
+
+//Zad3
+
+
+
+
+int main()
+{
+	////Zad1
+	//Struktura obiekt1;
+	//obiekt1 = { 34,57 };
+	//cout << obiekt1.x<<", "<<obiekt1.y;
+
+	
+
+
+}
